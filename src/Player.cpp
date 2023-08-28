e26f1f2a4d7cbb1455f0bc8ed6446c71e507107d
Apyre83 <fresnay.marc@gmail.com>
2023-08-28 23:33:35 +0200

    Added player movement

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,5 +1,39 @@
-//
-// Created by Leo on 28/08/2023.
-//
+#include "learn-for-me.h"
 
-#include "Player.hpp"
+void    Player::update(sf::Vector2u mapSize, int cellSizeX, int cellSizeY, int cellOffsetX, int cellOffsetY) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && x > speed) {
+        x -= speed;
+        isMoving = true;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && x < mapSize.x - speed - size) {
+        x += speed;
+        isMoving = true;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && y > speed) {
+        y -= speed;
+        isMoving = true;
+
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && y < mapSize.y - speed - size) {
+        y += speed;
+        isMoving = true;
+    }
+
+    if (!isMoving) /* Go to the closest cell */
+    {
+        int xCell = std::round(static_cast<float>(x) / cellSizeX);
+        int yCell = std::round(static_cast<float>(y) / cellSizeY);
+
+        xCell = std::max(0, std::min(static_cast<int>(mapSize.x / cellSizeX) - 1, xCell));
+        yCell = std::max(0, std::min(static_cast<int>(mapSize.y / cellSizeY) - 1, yCell));
+
+        x = xCell * cellSizeX + cellOffsetX * 1.0f + cellSizeX / 2 - size / 2;
+        y = yCell * cellSizeY + cellOffsetY * 1.0f + cellSizeY / 2 - size / 2;
+
+        std::cout << "xCell: " << xCell << " " << "yCell: " << yCell << std::endl;
+    }
+    else
+    {
+        isMoving = false;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,55 @@
-#include <SFML/Graphics.hpp>
+#include "learn-for-me.h"
+
 
 int main()
 {
-    // Create a window with the title "SFML Test"
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(desktop, "Learn for me", sf::Style::Fullscreen);
+    sf::Vector2u size = window.getSize();
 
-    // Create a circle shape with radius of 50
-    sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color::Green);
+    const int    cellSizeX = size.x / GRID_SIZE_X;
+    const int    cellSizeY = size.y / GRID_SIZE_Y;
+    const int    cellOffsetX = size.x - (cellSizeX * GRID_SIZE_X);
+    const int    cellOffsetY = size.y - (cellSizeY * GRID_SIZE_Y);
 
-    // Main loop
+    std::cout << "cellSizeX: " << cellSizeX << std::endl;
+    std::cout << "cellSizeY: " << cellSizeY << std::endl;
+    std::cout << "cellOffsetX: " << cellOffsetX << std::endl;
+    std::cout << "cellOffsetY: " << cellOffsetY << std::endl;
+
+    Player player(std::min(cellSizeX, cellSizeY));
+
     while (window.isOpen())
     {
-        // Process events
+
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+                window.close();
         }
 
-        // Clear the window
+        player.update(size, cellSizeX, cellSizeY, cellOffsetX, cellOffsetY);
+
         window.clear();
 
-        // Draw the circle shape
-        window.draw(shape);
+        for (int i = 0; i < GRID_SIZE_X; ++i) {
+            for (int j = 0; j < GRID_SIZE_Y; ++j) {
+                sf::RectangleShape cell(sf::Vector2f(cellSizeX, cellSizeY));
+                cell.setPosition({i * cellSizeX + cellOffsetX * 1.0f, j * cellSizeY + cellOffsetY * 1.0f});
 
-        // Display the contents of the window
+                cell.setFillColor(sf::Color::Black);
+                cell.setOutlineThickness(2);
+                cell.setOutlineColor(sf::Color::White);
+
+                window.draw(cell);
+            }
+        }
+
+        player.draw(window);
+
         window.display();
     }
 
